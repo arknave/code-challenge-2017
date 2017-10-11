731171674fc4684e58e3b6c256fc8f4474824080
Corbin Ford <corbin.ford@yahoo.com>
2017-10-11 12:23:26 -0400

    init project
    Project doc

---
+++ src/EventAnalyzer.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EventAnalyzer {
+	
+	// Implement solution here
+	public static String mostInterestedNeighborhood(ArrayList<Event> events) {
+		return null;
+	}
+	
+	//Implement solution here
+	public static String leastInterestedNeighborhood(ArrayList<Event> events) {
+		return null;
+	}
+	
+	//Implement solution here
+	public static String [] topIndividualsInterestedInLocations(ArrayList<Event> events) {
+		return null;
+	}
+	
+	//Implement solution here
+	public static String [] wheresWaldo (ArrayList<Event> events) {
+		return null;
+
+	}
+}
